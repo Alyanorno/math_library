a4f53f127acad7b3f4b5fee773b02ca269de1585
Alyanorno <Rickard.Fridvall@gmail.com>
2011-01-17 18:24:51 +0100

    Added some functionality to Vector, also found and fixed some bugs.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include "linear_math.h"
+#include <iostream>
+
+#define PRINT_VECTOR( v, n ) do { \
+	for( int i(0); i < n; i++ ) \
+		std::cout << v[i] << std::endl; \
+	std::cout << std::endl; } while(0)
 
 int main()
 {
 	add(2,5);
+
+	linear_math::Vector<3> v1( 3, 23, 50);
+	std::cout << "v1" << std::endl;
+	PRINT_VECTOR( v1, 3 );
+
+	linear_math::Vector<3> v2( -2, 1, 0.5 );
+	std::cout << "v2" << std::endl;
+	PRINT_VECTOR( v2, 3 );
+
+	std::cout << "Dot product of " << "v1" << " and " << "v2" << std::endl;
+	std::cout << v1.Dot( v2 ) << std::endl << std::endl;
+
+	v1.Normalize();
+	std::cout << "Normalize " << "v1" << std::endl;
+	PRINT_VECTOR( v1, 3 );
 	return 0;
 }
--- vector.h
+++ vector.h
@@ -4,14 +4,21 @@
 #define DEFAULT( N, T ) \
 public: \
 	Vector() { for(int i(0); i < N; i++) values[i] = 0; } \
-	void Normalize() {} \
-	T Length() {} \
-	T inline Dot( Vector<N, T>* v ) \
+	void Normalize() \
 	{ \
-		return dot::Loop<N-1, T>::function( values[i], v[i] ); /* = ( values[i], v[i] ) ? */ \
+		T n = values[0]; \
+		for( int i(1); i < N; i++ ) \
+			if( n < values[i] ) \
+				n = values[i]; \
+		n = 1 / n; \
+		for( int i(0); i < N; i++ ) \
+			values[i] *= n; \
 	} \
+	T Length() \
+		{ return sqrt( length::Loop<N-1, T>::function( &values ) ); } \
+	T inline Dot( Vector<N, T>& v ) \
+		{ return dot::Loop<N-1, T>::function( values, v.values ); } /* = ( values[i], v[i] ) ? */ \
 	T operator[]( int n ) { return values[n]; } \
-private: \
 	T values[N]
 
 
@@ -19,30 +26,44 @@ namespace linear_math
 {
 	namespace dot
 	{
-		template<int N, class T = float>
+		template< int N, typename T = float >
 		struct Loop
-			{ static inline T function( T* x, T* y ) 
-				{ return x[N] * y[N] + function<N-1, T>( x, y ); }};
-		template<class T>
+			{ static inline T function( T x[], T y[] ) 
+				{ return x[N] * y[N] + Loop<N-1, T>::function( x, y ); }};
+		template< typename T >
 		struct Loop<0, T>
-			{ static inline T function( T* x, T* y ) 
+			{ static inline T function( T x[], T y[] ) 
 				{ return x[0] * y[0]; }};
 	}
+	namespace length
+	{
+		template< int N, typename T = float >
+		struct Loop
+			{ static inline T function( T* v ) 
+				{ return v[n] * function<N-1, T>( v ); }};
+		template< typename T >
+		struct Loop<0, T>
+			{ static inline T function( T* v ) 
+				{ return v[0]; }};
+	}
 
-	template< int N, class T = float >
-	class Vector
+	template< int N, typename T = float >
+	struct Vector
 	{
 		DEFAULT( N, T);
 	};
 
-	template< class T >
-	class Vector<3, T>
+	template< typename T >
+	struct Vector<3, T>
 	{
 		DEFAULT( 3, T );
 	public:
-		Vector( T x, T y, T z ) : values[0](x),
-					  values[1](y).
-					  values[2](x) {}
+		Vector( T x, T y, T z ) 
+		{
+			values[0] = x;
+			values[1] = y;
+			values[2] = z;
+		}
 		Vector<3, T> Cross( Vector<3, T>* v ) {}
 	};
 }
