19aa0925b31d8013c033978292c0158c429b0201
Alyanorno <Rickard.Fridvall@gmail.com>
2011-01-17 23:28:43 +0100

    Added LOOP

--- vector.h
+++ vector.h
@@ -1,51 +1,41 @@
 #pragma once
 #include <math.h>
 
+
 #define DEFAULT( N, T ) \
 public: \
 	Vector() { for(int i(0); i < N; i++) values[i] = 0; } \
 	void Normalize() \
 	{ \
-		T n = values[0]; \
-		for( int i(1); i < N; i++ ) \
-			if( n < values[i] ) \
-				n = values[i]; \
-		n = 1 / n; \
+		T length = Length(); \
 		for( int i(0); i < N; i++ ) \
-			values[i] *= n; \
+			values[i] /= length; \
 	} \
 	T Length() \
-		{ return sqrt( length::Loop<N-1, T>::function( &values ) ); } \
+		{ return sqrt( length::Loop<N-1, T>::function( values ) ); } \
 	T inline Dot( Vector<N, T>& v ) \
 		{ return dot::Loop<N-1, T>::function( values, v.values ); } /* = ( values[i], v[i] ) ? */ \
 	T operator[]( int n ) { return values[n]; } \
 	T values[N]
 
+#define LOOP( TYPE_ARGS, ARGS, FUNCTION, OPERATION, ZERO ) \
+	template< int N, typename T = float > \
+	struct Loop \
+		{ static inline T function( TYPE_ARGS ) \
+			{ return FUNCTION OPERATION Loop<N-1, T>::function( ARGS ); }}; \
+	template< typename T > \
+	struct Loop<0, T> \
+		{ static inline T function( TYPE_ARGS ) \
+			{ return ZERO; }}
+#define COMMA ,
+
 
 namespace linear_math
 {
 	namespace dot
-	{
-		template< int N, typename T = float >
-		struct Loop
-			{ static inline T function( T x[], T y[] ) 
-				{ return x[N] * y[N] + Loop<N-1, T>::function( x, y ); }};
-		template< typename T >
-		struct Loop<0, T>
-			{ static inline T function( T x[], T y[] ) 
-				{ return x[0] * y[0]; }};
-	}
+		{ LOOP( T x[] COMMA T y[], x COMMA y, x[N] * y[N], +, x[0] * y[0] ); }
 	namespace length
-	{
-		template< int N, typename T = float >
-		struct Loop
-			{ static inline T function( T* v ) 
-				{ return v[n] * function<N-1, T>( v ); }};
-		template< typename T >
-		struct Loop<0, T>
-			{ static inline T function( T* v ) 
-				{ return v[0]; }};
-	}
+		{ LOOP( T v[], v, v[N] * v[N], +, v[0] * v[0] ); }
 
 	template< int N, typename T = float >
 	struct Vector
@@ -67,4 +57,6 @@ namespace linear_math
 		Vector<3, T> Cross( Vector<3, T>* v ) {}
 	};
 }
+#undef COMMA
+#undef LOOP
 #undef DEFAULT
