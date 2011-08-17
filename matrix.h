8127580c57d21988b18ed3cd511302b84c2cb5a2
Alyanorno <Rickard.Fridvall@gmail.com>
2011-08-17 02:27:01 +0200

    Changed the macros to not cover so much, and added the changes i made to the vector.h to matrix.h. Also found a problem with operator= because of the Base classes solved by adding concstructors that takes a VectorBase type.

--- matrix.h
+++ matrix.h
@@ -2,117 +2,123 @@
 #include "vector.h"
 #include <math.h>
 
-#define DEFAULT( M, N, T ) \
-public: \
-	Matrix() {} \
-	void Transpose() \
-	{ \
-		for( int i(0); i < N; i++ ) \
-			for ( int j(1); j < N; j++ )\
-			{ \
-				/* switch place */ \
-				T temp; \
-				temp = columns[i+j][i]; \
-				columns[i+j][i] = columns[i][i+j]; \
-				columns[i][i+j] = temp; \
-			} \
-	} \
-	void Identity() \
-	{ \
-		for( int i(0); i < N; i++ ) \
-		{ \
-			columns[i].Zero(); \
-			columns[i][i] = 1; \
-		} \
-	} \
-	Vector<N,T>& operator[]( int n ) { return columns[n]; } \
-	OPERATOR(+, M, N, T) \
-	OPERATOR(-, M, N, T) \
-	\
-	/* Matrix of same size */ \
-	friend Matrix<M,N,T> operator*( Matrix<M,N,T>& m1, Matrix<M,N,T>& m2 ) \
-	{ \
-		Matrix<M,N,T> result; \
-		Vector<N,T> v[M]; \
-		for( int i(0); i < M; i++ ) \
-			for( int j(0); j < N; j++ ) \
-				v[i][j] = m1[j][i]; \
-		for( int i(0); i < N; i++ ) \
-			for( int j(0); j < M; j++ ) \
-				result[i][j] = v[j].Dot( m2[i] ); \
-		return result; \
-	} \
-	void operator*=( Matrix<M,N,T>& m ) \
-	{ \
-		Vector<N,T> v[M]; \
-		for( int i(0); i < M; i++ ) \
-			for( int j(0); j < N; j++ ) \
-				v[i][j] = columns[j][i]; \
-		for( int i(0); i < N; i++ ) \
-			for( int j(0); j < M; j++ ) \
-				columns[i][j] = v[j].Dot( m[i] ); \
-	} \
-	/* Vector */ \
-	friend Matrix<M,N,T> operator*( Matrix<M,N,T>& m, Vector<N,T>& v ) \
-	{ \
-		Vector<N,T> result[M]; \
-		Vector<N,T> tempVector[M]; \
-		for( int i(0); i < M; i++ ) \
-			for( int j(0); j < N; j++ ) \
-				tempVector[i][j] = columns[j][i]; \
-		for( int i(0); i < N; i++ ) \
-				result[i] = v.Dot( tempVector[i] ); \
-		return result; \
-	} \
-	/* Scalar */ \
-	friend Matrix<M,N,T> operator*( Matrix<M,N,T>& m, T s ) \
-	{ \
-		Matrix<M,N,T> result; \
-		for( int i(0); i < N; i++ ) \
-				result[i] = m[i] * s; \
-		return result; \
-	} \
-	void operator*=( T s ) \
-		{ for( int i(0); i < N; i++ ) columns[i] *= s; } \
-	Vector<M,T> columns[N]
-
-#define OPERATOR( OPERATION, M, N, T ) \
-	/* Matrix of same size */ \
-	friend Matrix<M,N,T> operator OPERATION( Matrix<M,N,T>& m1, Matrix<M,N,T>& m2 ) \
-	{ \
-		Matrix<M,N,T> result; \
-		for( int i(0); i < N; i++ ) \
-			result[i] = m1[i] OPERATION m2[i]; \
-		return result; \
-	} \
-	void operator OPERATION=( Matrix<M,N,T>& m ) \
-		{ for( int i(0); i < N; i++ ) columns[i] OPERATION##= m[i]; } \
-	/* Vector */ \
-	friend Matrix<M,N,T> operator OPERATION( Matrix<M,N,T>& m, Vector<N,T>& v ) \
-	{ \
-		Matrix<M,N,T> result; \
-		for( int i(0); i < N; i++ ) \
-			result[i] = m[i] OPERATION v[i]; \
-		return result; \
-	} \
-	void operator OPERATION=( Vector<N,T>& v ) \
-		{ for( int i(0); i < N; i++ ) columns[i] OPERATION##= v[i]; }
-
 
 namespace linear_math
 {
-	template < int M, int N = M, typename T = float >
-	struct Matrix
+	namespace local
 	{
-		DEFAULT( M, N, T );
+		template < int M, int N, typename T >
+		struct MatrixBase
+		{
+			MatrixBase() {}
+			void Transpose()
+			{
+				for( int i(0); i < N; i++ )
+					for ( int j(1); j < N; j++ )
+					{
+						/* switch place */
+						T temp;
+						temp = columns[i+j][i];
+						columns[i+j][i] = columns[i][i+j];
+						columns[i][i+j] = temp;
+					}
+			}
+			void Identity()
+			{
+				for( int i(0); i < N; i++ )
+				{
+					columns[i].Zero();
+					columns[i][i] = 1;
+				}
+			}
+			Vector<N,T>& operator[]( int n ) { return columns[n]; }
+	
+			#define OPERATOR( OPERATION ) \
+				/* Matrix of same size */ \
+				friend MatrixBase<M,N,T> operator OPERATION( MatrixBase<M,N,T>& m1, MatrixBase<M,N,T>& m2 ) \
+				{ \
+					MatrixBase<M,N,T> result; \
+					for( int i(0); i < N; i++ ) \
+						result[i] = m1[i] OPERATION m2[i]; \
+					return result; \
+				} \
+				void operator OPERATION=( MatrixBase<M,N,T>& m ) \
+					{ for( int i(0); i < N; i++ ) columns[i] OPERATION##= m[i]; } \
+				/* Vector */ \
+				friend MatrixBase<M,N,T> operator OPERATION( MatrixBase<M,N,T>& m, Vector<N,T>& v ) \
+				{ \
+					MatrixBase<M,N,T> result; \
+					for( int i(0); i < N; i++ ) \
+						result[i] = m[i] OPERATION v[i]; \
+					return result; \
+				} \
+				void operator OPERATION=( Vector<N,T>& v ) \
+					{ for( int i(0); i < N; i++ ) columns[i] OPERATION##= v[i]; }
+			OPERATOR(+)
+			OPERATOR(-)
+			#undef OPERATOR
+	
+			/* Matrix of same size */
+			friend MatrixBase<M,N,T> operator*( MatrixBase<M,N,T>& m1, MatrixBase<M,N,T>& m2 )
+			{
+				MatrixBase<M,N,T> result;
+				Vector<N,T> v[M];
+				for( int i(0); i < M; i++ )
+					for( int j(0); j < N; j++ )
+						v[i][j] = m1[j][i];
+				for( int i(0); i < N; i++ )
+					for( int j(0); j < M; j++ )
+						result[i][j] = v[j].Dot( m2[i] );
+				return result;
+			}
+			void operator*=( MatrixBase<M,N,T>& m )
+			{
+ 				Vector<N,T> v[M];
+				for( int i(0); i < M; i++ )
+					for( int j(0); j < N; j++ )
+						v[i][j] = columns[j][i];
+				for( int i(0); i < N; i++ )
+					for( int j(0); j < M; j++ )
+						columns[i][j] = v[j].Dot( m[i] );
+			}
+			/* Vector */
+			friend MatrixBase<M,N,T> operator*( MatrixBase<M,N,T>& m, Vector<N,T>& v )
+			{
+				Vector<N,T> result[M];
+				Vector<N,T> tempVector[M];
+				for( int i(0); i < M; i++ )
+					for( int j(0); j < N; j++ )
+						tempVector[i][j] = columns[j][i];
+				for( int i(0); i < N; i++ )
+						result[i] = v.Dot( tempVector[i] );
+				return result;
+			}
+			/* Scalar */
+			friend MatrixBase<M,N,T> operator*( MatrixBase<M,N,T>& m, T s )
+			{
+				MatrixBase<M,N,T> result;
+				for( int i(0); i < N; i++ )
+						result[i] = m[i] * s;
+				return result;
+			}
+			void operator*=( T s )
+				{ for( int i(0); i < N; i++ ) columns[i] *= s; }
+			Vector<M,T> columns[N];
+		};
+	}
+	template < int M, int N = M, typename T = float >
+	struct Matrix : local::MatrixBase< M, N, T >
+	{
+		Matrix() {}
+		Matrix( MatrixBase& m ) { memcpy( this, &m, M*N*sizeof(T) ); }
 	};
 
 
 	template < typename T >
-	struct Matrix<3,3,T>
+	struct Matrix<3,3,T> : local::MatrixBase< 3, 3, T >
 	{
-		DEFAULT( 3, 3, T );
-	public:
+		Matrix() {}
+		Matrix( MatrixBase& m ) { memcpy( this, &m, 3*3*sizeof(T) ); }
 #define MATRIX_A columns[0][0]
 #define MATRIX_B columns[1][0]
 #define MATRIX_C columns[2][0]
@@ -153,10 +159,10 @@ namespace linear_math
 	};
 	
 	template < typename T >
-	struct Matrix<2,2,T>
+	struct Matrix<2,2,T> : local::MatrixBase< 2, 2, T > 
 	{
-		DEFAULT( 2, 2, T );
-	public:
+		Matrix() {}
+		Matrix( MatrixBase& m ) { memcpy( this, &m, 2*2*sizeof(T) ); }
 		Matrix<2,2,T> Inverse()
 		{
 			Matrix<2,2,T> result;
@@ -172,5 +178,3 @@ namespace linear_math
 	};
 }
 
-#undef OPERATOR
-#undef DEFAULT
--- vector.h
+++ vector.h
@@ -2,50 +2,28 @@
 #include <math.h>
 
 
-#define OPERATOR( OPERATION, N, T ) \
-	/* Vector of same size */ \
-	friend VectorBase<N,T> operator OPERATION( VectorBase<N,T>& v1, VectorBase<N,T>& v2 ) \
-	{ \
-		VectorBase<N,T> result; \
-		for(int i(0); i < N; i++ ) \
-			result[i] = v1[i] OPERATION v2[i]; \
-		return result; \
-	} \
-	void operator OPERATION=( VectorBase<N,T>& v ) \
-		{ for( int i(0); i < N; i++ ) values[i] OPERATION= v[i]; } \
-	/* Scalar */ \
-	friend VectorBase<N,T> operator OPERATION( VectorBase<N,T>& v1, T s ) \
-	{ \
-		VectorBase<N,T> result; \
-		for(int i(0); i < N; i++ ) \
-			result[i] = v1[i] OPERATION s; \
-		return result; \
-	} \
-	void operator OPERATION=( T s ) \
-		{ for( int i(0); i < N; i++ ) values[i] OPERATION= s; }
-
-#define LOOP( TYPE_ARGS, ARGS, FUNCTION, ZERO ) \
-	template< int N, typename T = float > \
-	struct Loop \
-		{ static inline T function( TYPE_ARGS ) \
-			{ FUNCTION Loop<N-1, T>::function( ARGS ); }}; \
-	template< typename T > \
-	struct Loop<0, T> \
-		{ static inline T function( TYPE_ARGS ) \
-			{ ZERO; }};
-#define COMMA ,
-
-
 namespace linear_math
 {
-	namespace dot
-		{ LOOP( T x[] COMMA T y[], x COMMA y, return x[N] * y[N] +, return x[0] * y[0] ) }
-	namespace length
-		{ LOOP( T v[], v, return v[N] * v[N] +, return v[0] * v[0] ) }
-		
-
 	namespace local
 	{
+		#define LOOP( TYPE_ARGS, ARGS, FUNCTION, ZERO ) \
+			template< int N, typename T = float > \
+			struct Loop \
+				{ static inline T function( TYPE_ARGS ) \
+					{ FUNCTION Loop<N-1, T>::function( ARGS ); }}; \
+			template< typename T > \
+			struct Loop<0, T> \
+				{ static inline T function( TYPE_ARGS ) \
+					{ ZERO; }};
+		#define COMMA ,
+		namespace dot
+			{ LOOP( T x[] COMMA T y[], x COMMA y, return x[N] * y[N] +, return x[0] * y[0] ) }
+		namespace length
+			{ LOOP( T v[], v, return v[N] * v[N] +, return v[0] * v[0] ) }
+		#undef COMMA
+		#undef LOOP
+		
+	
 		template< int N, typename T >
 		struct VectorBase
 		{
@@ -62,10 +40,34 @@ namespace linear_math
 			T inline Dot( VectorBase<N,T>& v )
 				{ return dot::Loop<N-1, T>::function( values, v.values ); }
 			T& operator[]( int n ) { return values[n]; }
-			OPERATOR(+, N, T)
-			OPERATOR(-, N, T)
-			OPERATOR(*, N, T)
-			OPERATOR(/, N, T)
+
+			#define OPERATOR( OPERATION ) \
+				/* Vector of same size */ \
+				friend VectorBase<N,T> operator OPERATION( VectorBase<N,T>& v1, VectorBase<N,T>& v2 ) \
+				{ \
+					VectorBase<N,T> result; \
+					for(int i(0); i < N; i++ ) \
+						result[i] = v1[i] OPERATION v2[i]; \
+					return result; \
+				} \
+				void operator OPERATION=( VectorBase<N,T>& v ) \
+					{ for( int i(0); i < N; i++ ) values[i] OPERATION= v[i]; } \
+				/* Scalar */ \
+				friend VectorBase<N,T> operator OPERATION( VectorBase<N,T>& v1, T s ) \
+				{ \
+					VectorBase<N,T> result; \
+					for(int i(0); i < N; i++ ) \
+						result[i] = v1[i] OPERATION s; \
+					return result; \
+				} \
+				void operator OPERATION=( T s ) \
+					{ for( int i(0); i < N; i++ ) values[i] OPERATION= s; }
+			OPERATOR(+)
+			OPERATOR(-)
+			OPERATOR(*)
+			OPERATOR(/)
+			#undef OPERATOR
+
 			T values[N];
 		};
 	}
@@ -74,6 +76,7 @@ namespace linear_math
 	struct Vector : local::VectorBase<N,T>
 	{
 		Vector() {}
+		Vector( VectorBase& v ) { memcpy( this, &v, N*sizeof(T) ); }
 	};
 
 
@@ -81,6 +84,7 @@ namespace linear_math
 	struct Vector<4,T> : local::VectorBase<4,T>
 	{
 		Vector() {}
+		Vector( VectorBase& v ) { memcpy( this, &v, 4*sizeof(T) ); }
 		Vector( T x, T y, T z, T w )
 		{
 			values[0] = x;
@@ -94,6 +98,7 @@ namespace linear_math
 	struct Vector<3,T> : local::VectorBase<3,T>
 	{
 		Vector() {}
+		Vector( VectorBase& v ) { memcpy( this, &v, 3*sizeof(T) ); }
 		Vector( T x, T y, T z ) 
 		{
 			values[0] = x;
@@ -114,6 +119,7 @@ namespace linear_math
 	struct Vector<2,T> : local::VectorBase<2,T>
 	{
 		Vector() {}
+		Vector( VectorBase& v ) { memcpy( this, &v, 2*sizeof(T) ); }
 		Vector( T x, T y )
 		{
 			values[0] = x;
@@ -121,6 +127,3 @@ namespace linear_math
 		}
 	};
 }
-#undef COMMA
-#undef LOOP
-#undef OPERATOR
